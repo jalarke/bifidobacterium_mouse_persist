8306f17ed0c71a799dbfffd2977a5490fc1b2fd9
Jules Larke <73035742+jalarke@users.noreply.github.com>
2023-12-01 13:06:59 -0800

    Update and rename fig_5A_C_E.R to fig_6A_C_E.R
    renamed for order in which figures appeared in publication

--- scripts/fig_6A_C_E.R
+++ scripts/fig_6A_C_E.R
@@ -1,6 +1,8 @@
 #Creating a differential log ratio plot to demonstrate log ratio difference
 #Butyrate, Propionate, Propanediol, Lactate,and High/Low Bifido
 
+load(file='../data/MouseMetaboliteManuscript.RData')
+
 #Propionate
 #data: propionate_map
 # Fig 5c
@@ -106,41 +108,3 @@ ggplot(data=propanediol_map, aes(x=Propanediol_category2, y= Bifido_LachnoRumino
   guides(fill=guide_legend("1,2-PD"))
 
 ggsave("songbird_propanediol.tiff", width = 2.5, height = 2.5, units = "in", dpi=600)
-
-#High/Low Bifido Categories
-#data: songbird_respond_final
-#Fig 3D
-View(songbird_respond_final)
-library(ggplot2)
-
-blab <- expression(paste("Log ", bgroup("(",italic(frac("Bifidobacteriaceae","Lachnospiraceae + Ruminococcaceae")),")"),sep = ""))
-labs <- c("HE", "ME")
-  ggplot(data=songbird_respond_final, aes(x=Responder2, y= Bifido_LachnoRumino, fill = Responder2)) + 
-  geom_boxplot(outlier.shape = 21, outlier.size = 3) +
-  #geom_dotplot(binaxis = 'y', stackdir = 'center', binwidth = 0.005) +
-  labs(y = blab, x = "Persistence") +
-  #scale_color_manual(values=c("#457A84", "#DCAE1D", "#0A3F63", "#6B6C6C"))
-  annotate("segment", x = 1, xend = 2, y = 9, yend = 9) +
-  annotate("text", x = 1.5, y = 9.25, label ="***", size = 4) +
-  scale_fill_manual(values = c("seagreen", "seagreen1"), labels=c("High", "Low")) +
-  expand_limits(x = 0.5) +
-    scale_x_discrete(labels = labs) +
-  theme_bw() +
-  theme(
-    title = element_text(size = 8),
-    axis.line.x = element_line(color = "black", size = .5),
-    axis.line.y = element_line(color = "black", size = .5),
-    axis.title.x = element_text(size = 8),
-    axis.title.y = element_text(size = 8),
-    axis.text.x = element_text(size = 8),
-    axis.ticks.x = element_blank(),
-    legend.title = element_text(size = 8),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    legend.position = "none",
-    panel.border = element_blank(),
-    panel.background = element_blank()
-  ) +
-  guides(fill=guide_legend(""))
-
-ggsave("3D.tiff", width = 3, height =3, units = "in", dpi=1000)
